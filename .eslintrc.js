6cc317187ebcb4318326ec7490a365cd9cc41698
swarajkhn <136063167+swarajkhn@users.noreply.github.com>
2024-06-05 02:46:42 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'jest/globals': true
-  },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+
+        'node': true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
-  },
-  'plugins': [
-    'react', 'jest'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 'error',
-    'react/prop-types': 0
-  }
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+    }
 }
